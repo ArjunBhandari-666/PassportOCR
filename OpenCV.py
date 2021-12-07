a59d1e1aa7311ea6764aceeb6ff0d76cb63d6f24
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2021-12-07 18:15:19 +0530

    Update OpenCV.py

--- OpenCV.py
+++ OpenCV.py
@@ -1,25 +1,31 @@
 import cv2
 import numpy as np
-import pytesseract
-from PIL import Image
-
-#src_path ="D:/python'/practice"
-
-def get_string(img_path):
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    kernel = np.ones((1, 1), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
-
-    cv2.imwrite("removed_noise.png", img)
-    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    cv2.imwrite("thres.png", img)
-    result = pytesseract.image_to_string(Image.open("thres.png"))
-
-    return result
-
-
-print ('------------TEXT------------')
-
-print (get_string("imag1.png"))
+ 
+cap = cv2.VideoCapture(0)
+ 
+while 1:
+    ret,frame =cap.read()
+    # ret will return a true value if the frame exists otherwise False
+    into_hsv =cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    # changing the color format from BGr to HSV
+    # This will be used to create the mask
+    L_limit=np.array([98,50,50]) # setting the blue lower limit
+    U_limit=np.array([139,255,255]) # setting the blue upper limit
+        
+ 
+    b_mask=cv2.inRange(into_hsv,L_limit,U_limit)
+    # creating the mask using inRange() function
+    # this will produce an image where the color of the objects
+    # falling in the range will turn white and rest will be black
+    blue=cv2.bitwise_and(frame,frame,mask=b_mask)
+    # this will give the color to mask.
+    cv2.imshow('Original',frame) # to display the original frame
+    cv2.imshow('Blue Detector',blue) # to display the blue object output
+ 
+    if cv2.waitKey(1)==27:
+        break
+    # this function will be triggered when the ESC key is pressed
+    # and the while loop will terminate and so will the program
+cap.release()
+ 
+cv2.destroyAllWindows()
