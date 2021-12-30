d4e701df0aef191aa0f8ab7fbdc68aa2b37519f5
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2021-12-30 19:51:27 +0530

    final

--- image-text.js
+++ image-text.js
@@ -1,29 +1,13 @@
 const tesseract = require("tesseract.js")
 const fs = require("fs")
 
-// tesseract.recognize("image-text.jpg", "eng", {
-//     logger: m => console.log(m)
-// }).then(result => {
-//     console.log(result.data.text)
-// }).catch(err => {
-//     console.log(err.message)
-// })
-
-// tesseract.recognize("image-hindi-text.jpg", "hin", {
-//     logger: m => console.log(m)
-// }).then(({ data: { text } }) => {
-//     console.log(text)
-// }).catch(err => {
-//     console.log(err.message)
-// })
-
-// tesseract.recognize("image-chinese-text.png", "chi_tra", {
-//     logger: m => console.log(m)
-// }).then(({ data: { text } }) => {
-//     console.log(text)
-// }).catch(err => {
-//     console.log(err.message)
-// })
+tesseract.recognize("image-text.jpg", "eng", {
+    logger: m => console.log(m)
+}).then(result => {
+    console.log(result.data.text)
+}).catch(err => {
+    console.log(err.message)
+})
 
 const getImageText = async (fileName, lang, logger) => {
     let recognizeResult = null
